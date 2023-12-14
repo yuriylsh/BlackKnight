9a9b4247e80e68aa766109622d1955af434c84e1
Yuriy Lyeshchenko <yuriydev@live.com>
2023-12-14 08:53:23 -0600

    Fixed Sync

--- Syncing/SyncDebug.cs
+++ Syncing/SyncDebug.cs
@@ -12,23 +12,27 @@ namespace DeveloperSample.Syncing
         public List<string> InitializeList(IEnumerable<string> items)
         {
             var bag = new ConcurrentBag<string>();
-            Parallel.ForEach(items, async i =>
+            Parallel.ForEachAsync(items, async (i, _) =>
             {
                 var r = await Task.Run(() => i).ConfigureAwait(false);
                 bag.Add(r);
-            });
+            }).Wait();  // typically we don't want to do Wait(), much prefer to make InitializeList async and await this call.
             var list = bag.ToList();
             return list;
         }
 
         public Dictionary<int, string> InitializeDictionary(Func<int, string> getItem)
         {
+            var degreeOfParallelism = 3;
             var itemsToInitialize = Enumerable.Range(0, 100).ToList();
+            var chunkSize = Math.DivRem(itemsToInitialize.Count, degreeOfParallelism, out var reminder);
+            if (reminder > 0) chunkSize += 1;
+            var chunks = itemsToInitialize.Chunk(chunkSize);
 
             var concurrentDictionary = new ConcurrentDictionary<int, string>();
-            var threads = Enumerable.Range(0, 3)
-                .Select(i => new Thread(() => {
-                    foreach (var item in itemsToInitialize)
+            var threads = chunks
+                .Select(itemsInChunk => new Thread(() => {
+                    foreach (var item in itemsInChunk)
                     {
                         concurrentDictionary.AddOrUpdate(item, getItem, (_, s) => s);
                     }
