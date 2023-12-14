9062355e2f0254b1da1d8c82ab5302366839c0ca
Yuriy Lyeshchenko <yuriydev@live.com>
2023-12-13 22:10:07 -0600

    Implementing container.

--- Container/Container.cs
+++ Container/Container.cs
@@ -1,10 +1,16 @@
 using System;
+using System.Collections.Generic;
 
-namespace DeveloperSample.Container
+namespace DeveloperSample.Container;
+
+// I'm only making existing tests to pass, no handling for edge cases or validation.
+// I assume adding test cases and adjusting the implementation accordingly will be the part of the 
+// follow-up technical interview...
+public class Container
 {
-    public class Container
-    {
-        public void Bind(Type interfaceType, Type implementationType) => throw new NotImplementedException();
-        public T Get<T>() => throw new NotImplementedException();
-    }
+    private static readonly Dictionary<Type, Type> Store = new();
+
+    public void Bind(Type interfaceType, Type implementationType)  => Store[interfaceType] = implementationType;
+
+    public T Get<T>() => (T)Activator.CreateInstance(Store[typeof(T)]);
 }
